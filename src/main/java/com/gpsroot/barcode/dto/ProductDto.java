e273ac2807add4d6e77a96a06ab8e810eef3834b
Orkhan Mammadov <orkhansmail@gmail.com>
2026-08-07 01:02:16 +0400

    yeni dto elave olundu

--- src/main/java/com/gpsroot/barcode/dto/ProductDto.java
+++ src/main/java/com/gpsroot/barcode/dto/ProductDto.java
@@ -2,11 +2,15 @@ package com.gpsroot.barcode.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class ProductDto {
 
     private String productName;
     private String barcode;
+    private LocalDateTime createdAt;
+    private List<ProductMovementDto> movements;
 
 }
--- src/main/java/com/gpsroot/barcode/mapper/ProductMapper.java
+++ src/main/java/com/gpsroot/barcode/mapper/ProductMapper.java
@@ -1,8 +1,10 @@
 package com.gpsroot.barcode.mapper;
 
 import com.gpsroot.barcode.dto.ProductDto;
+import com.gpsroot.barcode.dto.ProductMovementDto;
 import com.gpsroot.barcode.dto.ProductsName;
 import com.gpsroot.barcode.model.Product;
+import com.gpsroot.barcode.model.ProductMovement;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -10,7 +12,8 @@ public interface ProductMapper {
 
     ProductsName toProductsName(Product product);
 
-    Product toProduct(ProductDto productDto);
     ProductDto toProductDto(Product product);
+
+    ProductMovementDto toProductMovementDto(ProductMovement productMovement);
 
 }
